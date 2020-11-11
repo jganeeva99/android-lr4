1468f1a5b22120a650402021350d07eddc3b0024
jganeeva99 <49409956+jganeeva99@users.noreply.github.com>
2020-11-11 13:29:39 +0400

    Delete hook_task2.js

--- hook_task2.js
+++
@@ -1,7 +0,0 @@
-Java.perform(function () {
-    console.log("[*] Run instrumentation");
-    var MainActivity = Java.use('com.su.lab4.utils.RootUtil');
-        MainActivity.isRootAvailable.implementation = function () {
-            console.log('Done: bypassed');
-        };
-});

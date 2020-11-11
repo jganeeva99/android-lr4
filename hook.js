26311b3babe1cba1fff84c06361a094df7953950
jganeeva99 <49409956+jganeeva99@users.noreply.github.com>
2020-11-11 15:04:02 +0400

    Update hook.js

--- hook.js
+++ hook.js
@@ -17,4 +17,27 @@ Java.perform(function () {
     PasswordHack.checkPassword.overload("java.lang.String").implementation = function(str){
     return true;
     }
+
+    console.log('4');
+    //4
+    var pinner = Java.use("okhttp3.CertificatePinner");
+	pinner.check.overload("java.lang.String", "java.util.List").implementation = function(str1, str2) {
+                return;
+        }
+	pinner.check.overload("java.lang.String", "[Ljava.security.cert.Certificate;").implementation = function(str1, str2) {
+		return;
+	}
+
+
+//1
+	var pinBypassFragment = Java.use("com.su.lab4.fragments.PinBypassFragment");
+	var count = 0;
+	while (true) {
+	        var pin = count.toString();
+	        if (pinBypassFragment.checkPin(pin)){
+	            console.log(pin);
+	            break;
+	        }
+	        count = count + 1;
+	    }
 });
